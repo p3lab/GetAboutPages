aecc66844ce7722dd1e54525fdf5eddc2f81b0f6
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-01-27 15:31:46 -0800

    typo removed

--- tests/testthat/test-extract-about-links.R
+++ tests/testthat/test-extract-about-links.R
@@ -6,5 +6,3 @@ context("Extract about links")
 test_that("Extract about links should not return NA", {
           expect_equal(unique(extract_about_links("http://www.saltpondscoalition.org/")$href), "AboutUs.html")
 })
-
-GetAboutPages::if_not_about
