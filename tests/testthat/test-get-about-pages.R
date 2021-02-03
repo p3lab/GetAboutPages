851ec45373dc52b5f413c77e1163914614bb75d9
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-02-02 18:26:30 -0800

    Fix issue #3

--- tests/testthat/test-get-about-pages.R
+++ tests/testthat/test-get-about-pages.R
@@ -19,3 +19,7 @@ test_that("Extract about links should not return NA (with tree search)", {
 test_that("Get about page content returns a character vector (without tree search)", {
   expect_equal(class(get_about_page_content("https://snfagora.jhu.edu/")), "character")
 })
+
+test_that("Get about page from a website built by PHP", {
+  expect_equal(class(get_about_page_content("http://www.egacademyfoundation.org")), "character")
+})
